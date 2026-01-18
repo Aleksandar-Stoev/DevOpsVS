bc793ed3b930c08d1152388b16465e763fef0f92
Aleksandar-Stoev <sa6o_80@abv.bg>
2026-01-19 00:00:35 +0200

    fix conflict

--- calculator.java
+++ calculator.java
@@ -18,11 +18,8 @@ public class Calculator {
         return a - b;
     }
 
-<<<<<<< HEAD
     //change for fix from IDE
-=======
     //fix conflict from vsc
->>>>>>> b0dfdeba1e40ca64f3650f298fd4e4fcd67a256e
     public int multiply(int a, int b) {
         return a * b;
     }
